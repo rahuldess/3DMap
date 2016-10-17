1934ad14db0f4782c32d59b94ffb5906831577a8
eternity1030 <niti.sharma30@gmail.com>
2016-10-16 19:12:47 -0700

    Update svg_controller.rb

--- app/controllers/svg_controller.rb
+++ app/controllers/svg_controller.rb
@@ -5,12 +5,24 @@ class SvgController < ApplicationController
 
   def city_object
     {
-      paths: [  "#{City.sunnyvale}", "#{City.santa_clara}", "#{City.mountain_view}", "#{City.san_jose}",
-                "#{City.cupertino}", "#{City.campbell}", "#{City.saratoga}", "#{City.milpitas}"],
+      paths: [  "#{City.sunnyvale}",
+                "#{City.santa_clara}",
+                "#{City.mountain_view}",
+                "#{City.san_jose}",
+                "#{City.cupertino}",
+                "#{City.campbell}",
+                "#{City.saratoga}",
+                "#{City.milpitas}"],
         amounts: [ 50, 30, 100, 30, 20, 70, 99, 10 ],
         colors: [ '#0000A0', '#0000FF', 'green', 'maroon', 'brown', 'blue', 'white', 'orange' ],
-        info: [{ name: 'Sunnyvale' }, { name: 'Santa Clara' }, { name: 'Mountain View' }, { name: 'San Jose' },
-          { name: 'Cupertino'}, { name: 'Campbell'}, { name: 'Saratoga'}, { name: 'Milpitas'}],
+        info: [{ name: 'Sunnyvale' },
+                { name: 'Santa Clara' },
+                { name: 'Mountain View' },
+                { name: 'San Jose' },
+                { name: 'Cupertino'},
+                { name: 'Campbell'},
+                { name: 'Saratoga'},
+                { name: 'Milpitas'}],
         center: { x: 600, y: 150 }
       }
     end
