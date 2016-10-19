a41cb0a31da86e9f0177ee802da7b37f6ddfcfce
nsharma007 <niti.sharma@move.com>
2016-10-19 12:56:54 -0700

    added south_bay_support

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -9,17 +9,51 @@ class Statistics
     self.send(@module)
   end
 
+
+
   private
 
   def hot_area
-    data = fetch_geo_data(@requested_geo)
-    updated_data_set(data)
+    if @requested_geo.eql?("south_bay")
+      fetch_south_bay
+    else
+      data = fetch_geo_data(@requested_geo)
+      updated_data_set(data)
+    end
   end
 
   def fetch_geo_data(area)
     area_path = "#{Rails.root}/redis_stash/#{area}.json"
     file_ref    = File.open(area_path).read
     JSON.parse(file_ref, symbolize_names: true)
+  end
+
+  def fetch_south_bay
+    area_collection = []
+    Dir.glob("#{Rails.root}/redis_stash/*.json") do |file|
+      file_ref    = File.open(file).read
+      parsed_data = JSON.parse(file_ref, symbolize_names: true)
+      area_collection << south_bay_statistics(updated_data_set(parsed_data))
+    end
+    area_collection
+  end
+
+  def south_bay_statistics(data)
+    total_views = 0
+    total_leads = 0
+    total_shared = 0
+    total_saved = 0
+    data[:areas].each do |i|
+      total_views += i[:most_viewed]
+      total_leads += i[:leads_submitted]
+      total_shared += i[:shared]
+      total_saved += i[:saved]
+    end
+    data[:total_views] = total_views
+    data[:total_leads] = total_leads
+    data[:total_shared] = total_shared
+    data[:total_saved] = total_saved
+    data
   end
 
   def updated_data_set(data)
