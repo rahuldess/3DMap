8634801835367c3ce2f99a6361973f2efe1b80c3
Xin Liu <xin.liu@move.com>
2016-10-20 18:57:30 -0700

    math

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -84,12 +84,12 @@ $(document).ready(function() {
   }
 
   function plotCone() {
-    const Y_UNIT = 0.00268209219,
-      X_UNIT = 0.00935987756;
+    const Y_UNIT = 5.3898302533,
+      X_UNIT = 5.3823801302;
 
-    const PIVOT_POINT = [37.36261, -122.08903]; // (y , x)
-    const POVOT_POINT_SVG = [43.30236423376482, 69.81056448139134]; //(x, y)
-
+    const PIVOT_POINT = [37.34072, -121.93221]; // (y , x)
+    const POVOT_POINT_SVG = [484.378916507,1756.57331242]; //(x, y)
+    
     var points = [
       [37.3555465, -121.9586176],
       [37.4480379, -121.8487399]
@@ -100,9 +100,9 @@ $(document).ready(function() {
       var lng = points[i][1];
 
       var newY = POVOT_POINT_SVG[1] - (lat - PIVOT_POINT[0]) * Y_UNIT *
-        100000
+        1000
       var newX = POVOT_POINT_SVG[0] + (lng - PIVOT_POINT[1]) * X_UNIT *
-        100000
+        1000
 
       var coneGeometry = new THREE.ConeGeometry(28, 120, 60);
       var coneMaterial = new THREE.MeshBasicMaterial({
@@ -125,16 +125,15 @@ $(document).ready(function() {
        var lat = geoPoints[x]["lat_lng"][0];
        var lng = geoPoints[x]["lat_lng"][1];
 
-       const Y_UNIT = 0.05268209219,
-         X_UNIT = 0.05235987756;
+       const Y_UNIT = 5.3898302533,
+         X_UNIT = 5.3823801302;
 
-       const PIVOT_POINT = [37.36261, -122.08903]; // (y , x)
-       const POVOT_POINT_SVG = [43.30236423376482, 69.81056448139134]; //(x, y)
+       const PIVOT_POINT = [37.34072, -121.93221]; // (y , x)
+       const POVOT_POINT_SVG = [484.378916507,1756.57331242]; //(x, y)
 
-       var newY = POVOT_POINT_SVG[1] - (lat - PIVOT_POINT[0]) * Y_UNIT *
-         100000
-       var newX = POVOT_POINT_SVG[0] + (lng - PIVOT_POINT[1]) * X_UNIT *
-         100000
+       var newY = POVOT_POINT_SVG[1] - (lat - PIVOT_POINT[0]) * Y_UNIT * 1000
+       var newX = POVOT_POINT_SVG[0] + (lng - PIVOT_POINT[1]) * X_UNIT * 1000
+
 
 
        svgToPlot = "M," + newX + "," + newY + ", L," + (newX + 30) + "," + (
@@ -151,7 +150,7 @@ $(document).ready(function() {
        for (j = 0; j < simpleShapes.length; ++j) {
          simpleShape = simpleShapes[j];
          shape3d = simpleShape.extrude({
-           amount: 50,
+           amount: 100,
            bevelEnabled: false
          });
        }
@@ -161,10 +160,9 @@ $(document).ready(function() {
        mesh.geoInfo = geoPoints[x];
        mesh.rotation.x = Math.PI;
        mesh.scale.set(0.5635568066383669, 0.5635568066383669, 1);
-       // mesh.scale.set(1, 1, 1);
-       mesh.translateZ(-80 - 1);
+       mesh.translateZ(-100);
        mesh.translateX(-600);
-       mesh.translateY(-150);
+       mesh.translateY(-1000);
      } // end of for Loop
 
 
